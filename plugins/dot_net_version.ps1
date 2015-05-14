e04045af99527ff19b44e1be387d09feb6a3d649
Hounsou Dansou <hounsou.dansou@rackspace.com>
2015-05-14 16:58:00 -0500

    .net detection is posh v2 compatible

--- plugins/dot_net_version.ps1
+++ plugins/dot_net_version.ps1
@@ -1,18 +1,27 @@
+#requires -Version 1
 $provides = 'dot_net_version'
-function Collect-Data {
-    $output = New-Object System.Collections.Specialized.OrderedDictionary
-    $dot_net_version = New-Object System.Collections.Specialized.OrderedDictionary
+function Collect-Data 
+{
+    $output = New-Object -TypeName System.Collections.Specialized.OrderedDictionary
+    $dot_net_version = New-Object -TypeName System.Collections.Specialized.OrderedDictionary
     
-    $list_versions = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
-                            Get-ItemProperty -name Version -EA 0 |
-                            Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} |
-                            Select-Object @{Name='Name';Expression={$_.PSChildName}}, Version
+    $list_versions = Get-ChildItem -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
+    Get-ItemProperty -Name Version -EA 0 |
+    Where-Object -FilterScript {
+        $_.PSChildName -match '^(?!S)\p{L}'
+    } |
+    Select-Object -Property @{
+        Name       = 'Name'
+        Expression = {
+            $_.PSChildName
+        }
+    }, Version
 
-                     #(Get-ChildItem -Path $Env:windir\Microsoft.NET\Framework |
-                     #      Where-Object {$_.PSIsContainer -eq $true } |
-                     #      Where-Object {$_.Name -match 'v\d\.\d'} |
-                     #      Sort-Object -Property Name -Descending |
-                     #      Select-Object -First 1).Name
+    #(Get-ChildItem -Path $Env:windir\Microsoft.NET\Framework |
+    #      Where-Object {$_.PSIsContainer -eq $true } |
+    #      Where-Object {$_.Name -match 'v\d\.\d'} |
+    #      Sort-Object -Property Name -Descending |
+    #      Select-Object -First 1).Name
 
     $output.Add('dot_net_version',$list_versions)
     $output
